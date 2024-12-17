12e5a0bc8e28bea7f034b5a73259a511b8438d2a
Rastaar Haghi <rastaarhaghi@gmail.com>
2024-12-16 22:35:43 -0800

    Added Anchor accounts and instructions for Creating Channel and Creating Post

--- anchor/programs/OpenDialogueDapp/src/lib.rs
+++ anchor/programs/OpenDialogueDapp/src/lib.rs
@@ -1,70 +1,81 @@
-#![allow(clippy::result_large_err)]
-
 use anchor_lang::prelude::*;
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
-pub mod OpenDialogueDapp {
+pub mod open_dialogue_dapp {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseOpenDialogueDapp>) -> Result<()> {
-    Ok(())
-  }
+    // Create a channel
+    pub fn create_channel(ctx: Context<CreateChannel>, subject: Pubkey) -> Result<()> {
+      let channel = &mut ctx.accounts.channel;
+      channel.subject = subject;
+      channel.posts = Vec::new();
+      Ok(())
+    }
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.OpenDialogueDapp.count = ctx.accounts.OpenDialogueDapp.count.checked_sub(1).unwrap();
-    Ok(())
-  }
+    // Create a post in a channel
+    pub fn create_post(ctx: Context<CreatePost>, content: String) -> Result<()> {
+      let post = &mut ctx.accounts.post;
+      let author = &ctx.accounts.author;
+      let clock: Clock = Clock::get().unwrap();
 
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.OpenDialogueDapp.count = ctx.accounts.OpenDialogueDapp.count.checked_add(1).unwrap();
-    Ok(())
-  }
+      post.content = content;
+      post.timestamp = clock.unix_timestamp;
+      post.author = author.key();
+      post.channel = ctx.accounts.channel.key();
 
-  pub fn initialize(_ctx: Context<InitializeOpenDialogueDapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.OpenDialogueDapp.count = value.clone();
-    Ok(())
-  }
+      let channel = &mut ctx.accounts.channel;
+      channel.posts.push(post.key());
+      Ok(())
+    }
 }
 
 #[derive(Accounts)]
-pub struct InitializeOpenDialogueDapp<'info> {
+pub struct CreateChannel<'info> {
   #[account(mut)]
-  pub payer: Signer<'info>,
+  pub author: Signer<'info>,
 
-  #[account(
-  init,
-  space = 8 + OpenDialogueDapp::INIT_SPACE,
-  payer = payer
-  )]
-  pub OpenDialogueDapp: Account<'info, OpenDialogueDapp>,
+  #[account(init, payer = author, space = Channel::INIT_SPACE)]
+  pub channel: Account<'info, Channel>,
   pub system_program: Program<'info, System>,
 }
-#[derive(Accounts)]
-pub struct CloseOpenDialogueDapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
 
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub OpenDialogueDapp: Account<'info, OpenDialogueDapp>,
+#[derive(Accounts)]
+pub struct CreatePost<'info> {            
+    #[account(init, 
+        seeds = [
+            &channel.posts.len().to_be_bytes(), 
+            channel.key().as_ref()
+        ], 
+        bump, 
+        payer = author, 
+        space=Post::INIT_SPACE
+    )]
+    pub post: Account<'info, Post>,        
+    #[account(mut)]                                 
+    pub author: Signer<'info>,  
+    #[account(mut)]       
+    pub channel: Account<'info, Channel>,      
+    pub system_program: Program<'info, System>,    
 }
 
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub OpenDialogueDapp: Account<'info, OpenDialogueDapp>,
+#[account]
+pub struct Channel {
+  pub subject: Pubkey,
+  pub posts: Vec<Pubkey>
+}
+
+impl Channel {
+  pub const INIT_SPACE: usize = 8 + 32 + (4 + 25 * (4 + 64));
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct OpenDialogueDapp {
-  count: u8,
+pub struct Post {
+  pub channel: Pubkey,
+  #[max_len(128)]
+  pub content: String,
+  pub timestamp: i64,
+  pub author: Pubkey,
 }
