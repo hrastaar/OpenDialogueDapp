7459487b256698c88ebee77e6b2fb1c570f9d6e0
Joshua Blew <joshua@normalfinance.io>
2024-12-17 22:24:23 -0600

    refactor: frontend to display a channel, it's posts, and a create_post button

--- src/components/OpenDialogueDapp/OpenDialogueDapp-ui.tsx
+++ src/components/OpenDialogueDapp/OpenDialogueDapp-ui.tsx
@@ -1,13 +1,16 @@
-'use client'
+"use client";
 
-import { Keypair, PublicKey } from '@solana/web3.js'
-import { useMemo } from 'react'
-import { ellipsify } from '../ui/ui-layout'
-import { ExplorerLink } from '../cluster/cluster-ui'
-import { useOpenDialogueDappProgram, useOpenDialogueDappProgramAccount } from './OpenDialogueDapp-data-access'
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { useMemo } from "react";
+import { ellipsify } from "../ui/ui-layout";
+import { ExplorerLink } from "../cluster/cluster-ui";
+import {
+  useOpenDialogueDappProgram,
+  useOpenDialogueDappProgramAccount,
+} from "./OpenDialogueDapp-data-access";
 
 export function OpenDialogueDappCreate() {
-  const { initialize } = useOpenDialogueDappProgram()
+  const { initialize } = useOpenDialogueDappProgram();
 
   return (
     <button
@@ -15,50 +18,63 @@ export function OpenDialogueDappCreate() {
       onClick={() => initialize.mutateAsync(Keypair.generate())}
       disabled={initialize.isPending}
     >
-      Create {initialize.isPending && '...'}
+      Create {initialize.isPending && "..."}
     </button>
-  )
+  );
 }
 
 export function OpenDialogueDappList() {
-  const { accounts, getProgramAccount } = useOpenDialogueDappProgram()
+  const { accounts, getProgramAccount } = useOpenDialogueDappProgram();
 
   if (getProgramAccount.isLoading) {
-    return <span className="loading loading-spinner loading-lg"></span>
+    return <span className="loading loading-spinner loading-lg"></span>;
   }
   if (!getProgramAccount.data?.value) {
     return (
       <div className="alert alert-info flex justify-center">
-        <span>Program account not found. Make sure you have deployed the program and are on the correct cluster.</span>
+        <span>
+          Program account not found. Make sure you have deployed the program and
+          are on the correct cluster.
+        </span>
       </div>
-    )
+    );
   }
   return (
-    <div className={'space-y-6'}>
+    <div className={"space-y-6"}>
       {accounts.isLoading ? (
         <span className="loading loading-spinner loading-lg"></span>
       ) : accounts.data?.length ? (
         <div className="grid md:grid-cols-2 gap-4">
           {accounts.data?.map((account) => (
-            <OpenDialogueDappCard key={account.publicKey.toString()} account={account.publicKey} />
+            <OpenDialogueDappCard
+              key={account.publicKey.toString()}
+              account={account.publicKey}
+            />
           ))}
         </div>
       ) : (
         <div className="text-center">
-          <h2 className={'text-2xl'}>No accounts</h2>
+          <h2 className={"text-2xl"}>No accounts</h2>
           No accounts found. Create one above to get started.
         </div>
       )}
     </div>
-  )
+  );
 }
 
 function OpenDialogueDappCard({ account }: { account: PublicKey }) {
-  const { accountQuery, incrementMutation, setMutation, decrementMutation, closeMutation } = useOpenDialogueDappProgramAccount({
-    account,
-  })
+  const { accountQuery, createPostMutation } =
+    useOpenDialogueDappProgramAccount({
+      account,
+    });
 
-  const count = useMemo(() => accountQuery.data?.count ?? 0, [accountQuery.data?.count])
+  const count = useMemo(
+    () => accountQuery.data?.count ?? 0,
+    [accountQuery.data?.count]
+  );
+
+  const channel = {} as any; // TODO: fetch from program
+  const { subject, posts, post_count } = channel;
 
   return accountQuery.isLoading ? (
     <span className="loading loading-spinner loading-lg"></span>
@@ -66,57 +82,52 @@ function OpenDialogueDappCard({ account }: { account: PublicKey }) {
     <div className="card card-bordered border-base-300 border-4 text-neutral-content">
       <div className="card-body items-center text-center">
         <div className="space-y-6">
-          <h2 className="card-title justify-center text-3xl cursor-pointer" onClick={() => accountQuery.refetch()}>
-            {count}
+          {/* Header */}
+          <h2
+            className="card-title justify-center text-3xl cursor-pointer"
+            onClick={() => accountQuery.refetch()}
+          >
+            {subject} with {post_count} {post_count > 1 ? "posts" : "post"}
           </h2>
+
+          {/* Add post button */}
           <div className="card-actions justify-around">
             <button
               className="btn btn-xs lg:btn-md btn-outline"
-              onClick={() => incrementMutation.mutateAsync()}
-              disabled={incrementMutation.isPending}
-            >
-              Increment
-            </button>
-            <button
-              className="btn btn-xs lg:btn-md btn-outline"
               onClick={() => {
-                const value = window.prompt('Set value to:', count.toString() ?? '0')
-                if (!value || parseInt(value) === count || isNaN(parseInt(value))) {
-                  return
+                const postContent = window.prompt(
+                  "Set post content to:",
+                  count.toString() ?? "0"
+                );
+                if (!postContent || postContent === "") {
+                  return;
                 }
-                return setMutation.mutateAsync(parseInt(value))
+                return createPostMutation.mutateAsync(postContent);
               }}
-              disabled={setMutation.isPending}
+              disabled={createPostMutation.isPending}
             >
-              Set
-            </button>
-            <button
-              className="btn btn-xs lg:btn-md btn-outline"
-              onClick={() => decrementMutation.mutateAsync()}
-              disabled={decrementMutation.isPending}
-            >
-              Decrement
+              Create post
             </button>
           </div>
-          <div className="text-center space-y-4">
-            <p>
-              <ExplorerLink path={`account/${account}`} label={ellipsify(account.toString())} />
-            </p>
-            <button
-              className="btn btn-xs btn-secondary btn-outline"
-              onClick={() => {
-                if (!window.confirm('Are you sure you want to close this account?')) {
-                  return
-                }
-                return closeMutation.mutateAsync()
-              }}
-              disabled={closeMutation.isPending}
-            >
-              Close
-            </button>
+
+          {/* Posts */}
+          <div>
+            {posts.map((post: any, index: number) => {
+              return (
+                <div key={index}>
+                  <h3>{post.author}</h3>
+                  <p>{post.content}</p>
+                  <p>
+                    <i>
+                      Created at {new Date(post.timestamp).toLocaleDateString()}
+                    </i>
+                  </p>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
